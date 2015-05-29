8d8016a73266237c9cafcea9fbad073938e424f9
ivanovro <roman.ivanov.7@gmail.com>
2015-05-30 01:29:37 +0300

    utility methods

--- src/main/scala/rivanov/spark.handson/CsvDataFrames.scala
+++ src/main/scala/rivanov/spark.handson/CsvDataFrames.scala
@@ -1,57 +1,77 @@
 package rivanov.spark.handson
 
-import org.apache.spark.sql.{Row, DataFrame, SQLContext}
+import org.apache.spark.sql.{Column, Row, DataFrame, SQLContext}
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import org.apache.spark.SparkContext
 
 import scala.reflect.io.File
 
-class CsvDataFrames(sc: SparkContext, countriesFile: String, dataFile: String) {
+class CsvDataFrames(sc: SparkContext, countriesFile: String, dataFile: String, countryAlias: String = "c", dataAlias: String = "d") {
 
   val sqlContext = new SQLContext(sc)
 
   import CsvDataFrames._
   import sqlContext.implicits._
 
-  val countriesDF = csvDF(countriesFile, "countries").as("c")
+  //DataFrames
+  val countriesDF = csvDF(countriesFile, "countries").as(countryAlias)
+  val dataDF = csvDF(dataFile, "data").as(dataAlias)
 
-  val dataDF = csvDF(dataFile, "data").as("d")
+  //Data fields
+  val cCountryCode = s"$countryAlias.Country Code"
+  val shortName = s"$countryAlias.Short Name"
+  val region = s"$countryAlias.Region"
+  val longName = s"$countryAlias.Long Name"
+  val kWh = "kWh"
 
-  //country columns
-  val cCountryCode = $"c.Country Code"
-  val shortName = $"c.Short Name"
-  val region = $"c.Region"
-  val longName = $"c.Long Name"
-  val kWh = 'kWh
+  val _2005 = s"$dataAlias.2005"
+  val dCountryCode = s"$dataAlias.Country Code"
+  val indicatorName = s"$dataAlias.Indicator Name"
 
-  //data columns
-  val _2005 = $"d.2005"
-  val dCountryCode = $"d.Country Code"
-  val indicatorName = $"d.Indicator Name"
+  //Implicit helpers
+  implicit val toColumn: String => Column = s => s.col
 
+  implicit class ColumnConversions(column: String) {
+    def col: Column = Symbol(column)
 
-  def runRddApiQuery(take: Int = 5) = countriesDF.join(dataDF, cCountryCode === dCountryCode)
-    .filter(_2005 !== "")
-    .select(_2005.cast("bigint").as(kWh), shortName, region, longName, indicatorName)
-    .where(indicatorName === "Electricity production (kWh)")
-    .where((region !== "") && (region !== "World"))
-    .filter(longName !== "")
-    .groupBy(kWh, shortName, region, longName).max(kWh.name)
-    .orderBy(kWh.desc)
-    .take(take)
-    .map(Result.apply)
+    def notEmpty: Column = column.col !== ""
+
+    def intCast: Column = column.col.cast("bigint")
+  }
+
+  //Rules
+  val regionRestriction = region.notEmpty && (region.col !== "World")
+  val indicatorRestriction = indicatorName === "Electricity production (kWh)"
+  val notEmpty2005 = _2005.notEmpty
+  val notEmptyLongName = longName.notEmpty
 
 
-  def runMajorSqlQuery(take: Int = 5) = sqlContext.sql( """
-                                             |SELECT MAX(CAST(d.`2005` AS BIGINT)) AS `kWh`, c.`Short Name`, c.`Region`, c.`Long Name`
-                                             |    FROM data d JOIN countries c ON (d.`Country Code` = c.`Country Code`)
-                                             |      WHERE c.`Region` <> '' AND c.`Region` <> 'World'
-                                             |      AND c.`Long Name` <> ''
-                                             |      AND d.`2005` <> ''
-                                             |      AND d.`Indicator Name` = 'Electricity production (kWh)'
-                                             |      GROUP BY c.`Short Name`, c.`Region`, c.`Long Name`
-                                             |      ORDER BY kWh DESC
-                                           """.stripMargin).take(take).map(Result.apply)
+  //Query functions
+
+  def dataFramesQuery() =
+    countriesDF.join(dataDF, cCountryCode.col === dCountryCode.col)
+      .filter(notEmpty2005)
+      .filter(notEmptyLongName)
+      .select(_2005.intCast.as(kWh), shortName, region, longName, indicatorName)
+      .where(indicatorRestriction)
+      .where(regionRestriction)
+      .groupBy(kWh, shortName, region, longName)
+      .max(kWh)
+      .orderBy(kWh.desc)
+      .take(5)
+      .map(Result.apply)
+
+
+  def sqlQuery(take: Int = 5) = sqlContext.sql( """
+                                                          |SELECT MAX(CAST(d.`2005` AS BIGINT)) AS `kWh`, c.`Short Name`, c.`Region`, c.`Long Name`
+                                                          |    FROM data d JOIN countries c ON (d.`Country Code` = c.`Country Code`)
+                                                          |      WHERE c.`Region` <> '' AND c.`Region` <> 'World'
+                                                          |      AND c.`Long Name` <> ''
+                                                          |      AND d.`2005` <> ''
+                                                          |      AND d.`Indicator Name` = 'Electricity production (kWh)'
+                                                          |      GROUP BY c.`Short Name`, c.`Region`, c.`Long Name`
+                                                          |      ORDER BY kWh DESC
+                                                        """.stripMargin).take(take).map(Result.apply)
 
 
   private def csvDF(filePath: String, tableName: String, delimiter: String = "\\|"): DataFrame = {
--- src/test/scala/rivanov/spark.handson/JoinDataFramesSpec.scala
+++ src/test/scala/rivanov/spark.handson/JoinDataFramesSpec.scala
@@ -17,15 +17,17 @@ class JoinDataFramesSpec extends Specification {
       val sc: SparkContext = new SparkContext("local[*]", "JoinDF", new SparkConf())
       val app = new CsvDataFrames(sc, countriesFile, dataFile)
 
-//      val results = app.runMajorSqlQuery()
-      val results = app.runRddApiQuery()
+      val sqlResults = app.sqlQuery()
+      val dfResults = app.dataFramesQuery()
 
-      results.length must_== 5
-      results.foldLeft((true, Long.MaxValue))((a, x) => (a._2 > x.kWh, x.kWh))._1 must beTrue
-      results.head.countryName.toString must_== "United States"
+      sqlResults mustEqual dfResults
+
+      dfResults.length must_== 5
+      dfResults.foldLeft((true, Long.MaxValue))((a, x) => (a._2 > x.kWh, x.kWh))._1 must beTrue
+      dfResults.head.countryName.toString must_== "United States"
 
       println(f"${"kWh"}%15s ${"Country"}%25s ${"Region"}%25s ${"Long Name"}%30s")
-      results.foreach(r => println(f"${r.kWh}%15s ${r.countryName}%25s ${r.region}%25s ${r.longCountryName}%30s"))
+      dfResults.foreach(r => println(f"${r.kWh}%15s ${r.countryName}%25s ${r.region}%25s ${r.longCountryName}%30s"))
 
       sc.stop()
       success

7477cb8f70302b74e1f85dee71f9d6530b0d8065
ivanovro <roman.ivanov.7@gmail.com>
2015-05-30 01:33:08 +0300

    minor refactoring

--- src/main/scala/rivanov/spark.handson/CsvDataFrames.scala
+++ src/main/scala/rivanov/spark.handson/CsvDataFrames.scala
@@ -14,10 +14,13 @@ class CsvDataFrames(sc: SparkContext, countriesFile: String, dataFile: String, c
   import sqlContext.implicits._
 
   //DataFrames
+
   val countriesDF = csvDF(countriesFile, "countries").as(countryAlias)
   val dataDF = csvDF(dataFile, "data").as(dataAlias)
 
+
   //Data fields
+
   val cCountryCode = s"$countryAlias.Country Code"
   val shortName = s"$countryAlias.Short Name"
   val region = s"$countryAlias.Region"
@@ -28,7 +31,9 @@ class CsvDataFrames(sc: SparkContext, countriesFile: String, dataFile: String, c
   val dCountryCode = s"$dataAlias.Country Code"
   val indicatorName = s"$dataAlias.Indicator Name"
 
+
   //Implicit helpers
+
   implicit val toColumn: String => Column = s => s.col
 
   implicit class ColumnConversions(column: String) {
@@ -39,7 +44,9 @@ class CsvDataFrames(sc: SparkContext, countriesFile: String, dataFile: String, c
     def intCast: Column = column.col.cast("bigint")
   }
 
+
   //Rules
+
   val regionRestriction = region.notEmpty && (region.col !== "World")
   val indicatorRestriction = indicatorName === "Electricity production (kWh)"
   val notEmpty2005 = _2005.notEmpty
@@ -73,6 +80,8 @@ class CsvDataFrames(sc: SparkContext, countriesFile: String, dataFile: String, c
                                                           |      ORDER BY kWh DESC
                                                         """.stripMargin).take(take).map(Result.apply)
 
+
+  // Initialization utils
 
   private def csvDF(filePath: String, tableName: String, delimiter: String = "\\|"): DataFrame = {
 
